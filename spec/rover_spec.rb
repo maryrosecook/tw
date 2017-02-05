ae2daeac4b4f2d35887ac59ea6a50e789c6a5b7f
Thomas William Page <tom.w.page@gmail.com>
2017-02-05 12:55:31 +0100

    rover_spec refactored

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -5,24 +5,40 @@ describe Rover do
   before(:each) do
     @rover1 = Rover.new('rover1',1,3,0)
     @rover2 = Rover.new('rover2',3,3,1)
-    @rover3 = Rover.new('rover1',1,3,2)
-    @rover4 = Rover.new('rover2',3,3,3)
-    @rover5 = Rover.new('rover2',3,3,'N')
+    @rover3 = Rover.new('rover3',1,3,2)
+    @rover4 = Rover.new('rover4',3,3,3)
+    @rover5 = Rover.new('rover5',3,3,'N')
     @plateu = Plateu.new(5,5)
   end
 
   describe 'Checks initialisation.' do
-    it 'Specifically the co-ordinates of rover when initialised.' do
+    it 'Specifically the co-ordinates of rover.x and rover.y when initialised.' do
       expect(@rover1.x).to eq 1
       expect(@rover2.x).to eq 3
       expect(@rover1.y).to eq 3
       expect(@rover2.y).to eq 3
-      expect(@rover1.compass_no).to eq 0
-      expect(@rover1.name).to eq 'rover1'
-      expect(@rover2.name).to eq 'rover2'
-
     end
   end
+
+  describe 'Checks initialisation.' do
+    it 'Specifically the co-ordinates of rover.compass_no when initialised.' do
+      expect(@rover1.compass_no).to eq 0
+      expect(@rover2.compass_no).to eq 1
+      expect(@rover3.compass_no).to eq 2
+      expect(@rover4.compass_no).to eq 3
+    end
+  end
+
+  describe 'Checks initialisation.' do
+    it 'Specifically the co-ordinates of rover.name when initialised.' do
+      expect(@rover1.name).to eq 'rover1'
+      expect(@rover2.name).to eq 'rover2'
+      expect(@rover3.name).to eq 'rover3'
+      expect(@rover4.name).to eq 'rover4'
+    end
+  end
+
+
 
   describe 'Checks initialisation.' do
     it 'Specifically the co-ordinates of rover when initialised.' do
@@ -59,19 +75,6 @@ describe Rover do
     end
   end
 
-  describe 'Checks #move method' do
-    it 'Expects and error message when the bot reaches the edge of the plateu' do
-      @rover1.y = 5
-      @rover2.x = 5
-      @rover3.y = 0
-      @rover4.x = 0
-      expect{ @rover1.move(@plateu) }.to raise_error "I'm on the edge of the plateu! Turn me around!"
-      expect{ @rover2.move(@plateu) }.to raise_error "I'm on the edge of the plateu! Turn me around!"
-      expect{ @rover3.move(@plateu) }.to raise_error "I'm on the edge of the plateu! Turn me around!"
-      expect{ @rover4.move(@plateu) }.to raise_error "I'm on the edge of the plateu! Turn me around!"
-    end
-  end
-
 
   describe 'Checks #status method' do
     it 'Expects the rover to return its position' do
@@ -83,6 +86,16 @@ describe Rover do
   describe 'Checks #conversion method' do
     it 'Expects the letter for the init position to be converted to a number' do
       expect { @rover5.compass_no_conversion }.to change{@rover5.compass_no}.from('N').to(0)
+    end
+  end
+
+
+  describe 'Checks #move method' do
+    it 'Expects and error message when the bot reaches the edge of the plateu' do
+      expect{ 4.times{@rover1.move(@plateu)} }.to raise_error "I'm on the edge of the plateu! Turn me around!"
+      expect{ 4.times{@rover2.move(@plateu)} }.to raise_error "I'm on the edge of the plateu! Turn me around!"
+      expect{ 4.times{@rover3.move(@plateu)} }.to raise_error "I'm on the edge of the plateu! Turn me around!"
+      expect{ 4.times{@rover4.move(@plateu)} }.to raise_error "I'm on the edge of the plateu! Turn me around!"
     end
   end
 
